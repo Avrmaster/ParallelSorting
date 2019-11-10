199a5aa6e6092d8b4628380011df7e2d72e3053c
Oleksandr Leskiv <oleksandr.leskiv@digia.co>
2019-11-10 23:19:03 +0200

    counting sort

--- main.py
+++ main.py
@@ -1,61 +1,28 @@
-from multiprocessing import Pool
-from functools import partial
-from random import randint
+from numpy.random import randint
 from time import time
-import operator
 
-from sorts import radix_sort
+from sorts import radix_sort, counting_sort
 
-
-def do_merge(all_results):
-    pointers = [0] * len(all_results)
-    lengths = [len(r) for r in all_results]
-
-    res = [None] * len(all_results)
-    values = [a[p] for (p, a) in zip(pointers, all_results)]
-    while True:
-        min_index, min_value = min(enumerate(values), key=operator.itemgetter(1))
-        pointers[min_index] += 1
-
-        if pointers[min_index] >= lengths[min_index]:
-            values.pop(min_index)
-            if len(values) == 0:
-                break
-        else:
-            values[min_index] = all_results[min_index][pointers[min_index]]
-
-        res.append(min_value)
-    return res
-
-
-def do_paralil(array, sort_function, processors_count):
-    array_len = len(array)
-    part_size, rem = divmod(array_len, processors_count)
-
-    with Pool(processes=processors_count) as pool:
-        sub_arrays = [array[(part_size * i):(part_size * (i + 1))] for i in range(processors_count)]
-        if rem > 0:
-            sub_arrays.append(array[part_size * processors_count - rem:array_len])
-
-        max_value = max(array)
-        return do_merge(pool.map(func=partial(sort_function, max_value=max_value), iterable=sub_arrays))
-
-
+max_value = 9
 times = {}
-for proc in range(1, 9):
-    for arr_size in [500000]:
-        arr = [randint(0, 2000) for i in range(arr_size)]
+for proc in range(1, 17):
+    # for arr_size in [20]:
+    for arr_size in [30000000]:
+        arr = randint(0, max_value, size=arr_size).tolist()
 
+        print('\rsorting..', end='')
         start = time()
-        do_paralil(arr, radix_sort, proc)
+        res = counting_sort(arr, 0, proc)
+        # res = radix_sort(arr, max_value, proc)
         end = time()
+
+        # print('Result', res)
 
         delta = end - start
         times[proc] = delta
-
         speed_up = times[1] / delta
         efficiency = speed_up / proc
 
-        print(f'Sorting on {proc} processor(s) took {abs(start - end)} second(s)\n'
+        print(f'\rSorting on {proc} processor(s) took {abs(start - end)} second(s)\n'
               f'Boost: {speed_up}x\n'
               f'Efficiency: {efficiency}\n\n')
--- sorts.py
+++ sorts.py
@@ -1,51 +1,66 @@
-def counting_sort(arr, max_value, get_index):
-    counts = [0] * max_value
-
-    # Counting - O(n)
-    for a in arr:
-        counts[get_index(a)] += 1
-
-    # Accumulating - O(k)
-    for i, c in enumerate(counts):
-        if i == 0:
-            continue
-        else:
-            counts[i] += counts[i - 1]
-
-    # Calculating start index - O(k)
-    for i, c in enumerate(counts[:-1]):
-        if i == 0:
-            counts[i] = 0
-        counts[i + 1] = c
-
-    res = [None] * len(arr)
-    # Sorting - O(n)
-    for a in arr:
-        index = counts[get_index(a)]
-        res[index] = a
-        counts[get_index(a)] += 1
-
-    return res
+from functools import partial, reduce
+from multiprocessing import Pool
+import numpy as np
 
 
 def get_digit(n, d):
-    for i in range(d - 1):
-        n //= 10
-    return n % 10
+    return (n // (10 ** d)) % 10
 
 
-def get_num_difit(n):
-    i = 0
-    while n > 0:
-        n //= 10
-        i += 1
-    return i
+def do_count(array, d):
+    counts = [0] * 10
+    for a in array:
+        counts[get_digit(a, d)] += 1
+    return counts
 
 
-def radix_sort(arr, max_value):
-    num_digits = get_num_difit(max_value)
-    # O(k(n+k))
+def populate(arg):
+    digit, times = arg
+    return np.full(times, fill_value=digit, dtype=int)
+
+
+def counting_sort(array, d, processors_count):
+    array_len = len(array)
+    part_size, rem = divmod(array_len, processors_count)
+
+    np_counts: np.ndarray = np.zeros(10, dtype=int)
+    with Pool(processes=processors_count) as pool:
+        sub_arrays = [array[(part_size * i):(part_size * (i + 1))] for i in range(processors_count)]
+        if rem > 0:
+            sub_arrays[0].extend(array[-rem:])
+
+        # print(processors_count, [len(a) for a in sub_arrays])
+
+        all_counts = pool.map(partial(do_count, d=d), sub_arrays)
+        for c in all_counts:
+            np_counts += np.array(c)
+
+        counts: list = np_counts.tolist()
+
+        # # accumulating
+        # for i in range(1, len(counts)):
+        #     counts[i] += counts[i - 1]
+        #
+        # # right shifting
+        # for i in reversed(range(1, len(counts))):
+        #     counts[i] = counts[i - 1]
+        # counts[0] = 0
+
+        # res = [None] * len(array)
+
+        results = pool.map(populate, enumerate(counts))
+        return np.concatenate(results).tolist()
+
+
+def radix_sort(array, max_value, processors_count):
+    num_digits = 0
+    while max_value > 0:
+        max_value //= 10
+        num_digits += 1
+
     for d in range(num_digits):
-        # Counting sort takes O(n+k)
-        arr = counting_sort(arr, max_value, lambda a: get_digit(a, d + 1))
-    return arr
+        array = counting_sort(
+            array, d=d,
+            processors_count=processors_count,
+        )
+    return array
